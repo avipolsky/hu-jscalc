03a508a8de57b74c453615be30e852ebe1586632
avipolsky <avi.polsky@gmail.com>
2024-10-28 22:37:18 +0200

    js-calc-20241028

---
+++ js/calc.js
@@ -0,0 +1,24 @@
+//basic calculator - avi polsky 28/10/2024
+
+let num1, num2, oper, answer;
+
+num1 = Number(prompt('Enter first #'))
+oper = prompt('Enter operator (+, -, * ,/ ): ')
+num2 = Number(prompt('Enter second #'))
+
+if (operator == '+') {
+    answer = number1 + number2;
+}
+else if (operator == '-') {
+    answer = number1 - number2;
+}
+else if (operator == '*') {
+    answer = number1 * number2;
+}
+else if {
+    answer = number1 / number2;
+}
+else  {
+    answer = oper + " is NOT a valid choice!";
+}
+alert(answer)
